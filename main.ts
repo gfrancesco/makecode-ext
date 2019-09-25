73657ca62c38dda023c4ed7ff0c4bad0959490c2
gfrancesco <gfrancesco@users.noreply.github.com>
2019-09-25 14:03:10 +0200

    UPD: main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Provides access to tiles toolkit.
+ */
+
+//% weight=100 color=#0fbc11 icon="\uf0c3"
+namespace prova {
+
+
+    /**
+     * This is a reporter block that returns a number
+     */
+    //% block
+    export function tedfddmp(): number {
+        return input.temperature();
+    }
+
+
+
+}
